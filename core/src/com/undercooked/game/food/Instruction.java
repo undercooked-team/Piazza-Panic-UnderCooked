dd5e9ea93d8906596355ecd7cc9f2f75697a4f30
OwenTho <owen_thomas@btinternet.com>
2023-04-29 23:41:18 +0100

    JavaDocs for Instruction complete.

--- core/src/com/undercooked/game/food/Instruction.java
+++ core/src/com/undercooked/game/food/Instruction.java
@@ -4,23 +4,51 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.JsonValue;
 import com.undercooked.game.assets.TextureManager;
 
+/**
+ * The class for one {@link Instruction} of a {@link Request}, which
+ * is used to instruct the player on what to do.
+ */
 public class Instruction {
+    /** The {@link Texture} to show. */
     private Texture texture;
+
+    /** The path for the {@link #texture}. */
     public String texturePath;
+
+    /** The text to display. */
     public String text;
 
+    /**
+     * Post loading of the {@link Instruction} to set the
+     * {@link Texture}.
+     * @param textureManager {@link TextureManager} : The {@link TextureManager} to use.
+     */
     public void postLoad(TextureManager textureManager) {
         texture = textureManager.getAsset(texturePath);
     }
 
-    public void load(TextureManager textureManager, String textureID) {
-        textureManager.loadAsset(textureID, texturePath, "textures");
+    /**
+     * Loading of the {@link Instruction} to tell the {@link TextureManager}
+     * to load the {@link #texturePath}.
+     * @param textureManager {@link TextureManager} : The {@link TextureManager} to use.
+     * @param textureGroup {@link String} : The group to load the {@link #texturePath} to.
+     */
+    public void load(TextureManager textureManager, String textureGroup) {
+        textureManager.loadAsset(textureGroup, texturePath, "textures");
     }
 
+    /**
+     * @return {@link Texture} : The {@link #texture}
+     */
     public Texture getTexture() {
         return texture;
     }
 
+    /**
+     * Converts the {@link Instruction} into a {@link JsonValue} to
+     * be stored in memory.
+     * @return {@link JsonValue} : The {@link Instruction} in Json format.
+     */
     public JsonValue serial() {
         JsonValue instructionRoot = new JsonValue(JsonValue.ValueType.object);
         instructionRoot.addChild("texture_path", new JsonValue(texturePath));
@@ -28,6 +56,11 @@ public class Instruction {
         return instructionRoot;
     }
 
+    /**
+     * Takes a {@link JsonValue} and sets the values of the
+     * {@link Instruction}.
+     * @param instructionRoot {@link JsonValue} : The {@link JsonValue} to load.
+     */
     public void deserialise(JsonValue instructionRoot) {
         texturePath = instructionRoot.getString("texture_path");
         text = instructionRoot.getString("text");
