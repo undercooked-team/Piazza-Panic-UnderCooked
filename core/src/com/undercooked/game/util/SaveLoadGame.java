f9ae0eab9eb32bb50f4fe059827cc9ac70d91130
OwenTho <owen_thomas@btinternet.com>
2023-04-30 09:23:06 +0100

    JavaDocs for SaveLoadGame and Register complete.

--- core/src/com/undercooked/game/util/SaveLoadGame.java
+++ core/src/com/undercooked/game/util/SaveLoadGame.java
@@ -12,17 +12,38 @@ import com.undercooked.game.logic.ScenarioLogic;
 import com.undercooked.game.map.Map;
 import com.undercooked.game.station.StationController;
 
+/**
+ * A class that simply saves and loads a game.
+ */
 public class SaveLoadGame {
 
+	/**
+	 * Saves the {@link JsonValue} produced by the
+	 * {@link GameLogic#serialise()} function.
+	 *
+	 * @param gameLogic {@link GameLogic} : The {@link GameLogic} to save.
+	 */
 	public static void saveGame(GameLogic gameLogic) {
 		JsonValue save = gameLogic.serialise();
 		FileControl.saveJsonData("save.json", save);
 	}
 
+	/**
+	 * Loads the save {@link JsonValue} and returns it.
+	 *
+	 * @return {@link JsonValue} : The save data.
+	 */
 	public static JsonValue loadGameJson() {
 		return FileControl.loadJsonData("save.json");
 	}
 
+	/**
+	 * Loads a {@link JsonValue} of save data, and loads it into
+	 * a {@link GameLogic}.
+	 *
+	 * @param gameLogic {@link GameLogic} : The {@link GameLogic} to load to.
+	 * @param saveData {@link JsonValue} : The save Json.
+	 */
 	public static void loadGame(GameLogic gameLogic, JsonValue saveData) {
 		gameLogic.deserialise(saveData);
 	}
