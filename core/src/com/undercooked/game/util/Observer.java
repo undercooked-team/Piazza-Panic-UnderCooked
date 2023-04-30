875d89a5e75020e7eaa218702fb45a8b1d98222f
OwenTho <owen_thomas@btinternet.com>
2023-04-30 08:47:08 +0100

    JavaDocs for Listener and Observer complete.

--- core/src/com/undercooked/game/util/Observer.java
+++ core/src/com/undercooked/game/util/Observer.java
@@ -1,5 +1,14 @@
 package com.undercooked.game.util;
 
+/**
+ * Observer class to be initialized wherever it is needed
+ * through an anonymous class defining the {@link #observe()}
+ * function return a value.
+ * @param <T> The type to return.
+ */
 public abstract class Observer<T> {
+    /**
+     * Return a {@link T} value to what is observing.
+     */
     public abstract  T observe();
 }
