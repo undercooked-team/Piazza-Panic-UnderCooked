251c0d467f7f0c5d9a7130f9b5af609e6b849542
OwenTho <owen_thomas@btinternet.com>
2023-05-02 20:17:39 +0100

    Updated the window title of the game.

--- desktop/src/com/undercooked/game/DesktopLauncher.java
+++ desktop/src/com/undercooked/game/DesktopLauncher.java
@@ -17,7 +17,7 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("eng1team3game");
+		config.setTitle("Piazza Panic: Undercooked");
 		// config.setWindowedMode(1920,1080);
 		// config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		new Lwjgl3Application(new MainGameClass(), config);
