95acecd7c02471a6504312ff53490226ad646aec
OwenTho <owen_thomas@btinternet.com>
2023-05-02 20:26:00 +0100

    Re-indented various classes that had their spacing replaced with tab characters, resulting in the style check erroring.

--- core/src/com/undercooked/game/food/Instruction.java
+++ core/src/com/undercooked/game/food/Instruction.java
@@ -9,75 +9,75 @@ import com.undercooked.game.assets.TextureManager;
  * is used to instruct the player on what to do.
  */
 public class Instruction {
-	/**
-	 * The {@link Texture} to show.
-	 */
-	private Texture texture;
+  /**
+   * The {@link Texture} to show.
+   */
+  private Texture texture;
 
-	/**
-	 * The path for the {@link #texture}.
-	 */
-	public String texturePath;
+  /**
+   * The path for the {@link #texture}.
+   */
+  public String texturePath;
 
-	/**
-	 * The text to display.
-	 */
-	public String text;
+  /**
+   * The text to display.
+   */
+  public String text;
 
-	/**
-	 * Post loading of the {@link Instruction} to set the
-	 * {@link Texture}.
-	 *
-	 * @param textureManager {@link TextureManager} : The {@link TextureManager} to
-	 *                       use.
-	 */
-	public void postLoad(TextureManager textureManager) {
-		texture = textureManager.getAsset(texturePath);
-	}
+  /**
+   * Post loading of the {@link Instruction} to set the
+   * {@link Texture}.
+   *
+   * @param textureManager {@link TextureManager} : The {@link TextureManager} to
+   *                       use.
+   */
+  public void postLoad(TextureManager textureManager) {
+    texture = textureManager.getAsset(texturePath);
+  }
 
-	/**
-	 * Loading of the {@link Instruction} to tell the {@link TextureManager}
-	 * to load the {@link #texturePath}.
-	 *
-	 * @param textureManager {@link TextureManager} : The {@link TextureManager} to
-	 *                       use.
-	 * @param textureGroup   {@link String} : The group to load the
-	 *                       {@link #texturePath} to.
-	 */
-	public void load(TextureManager textureManager, String textureGroup) {
-		textureManager.loadAsset(textureGroup, texturePath, "textures");
-	}
+  /**
+   * Loading of the {@link Instruction} to tell the {@link TextureManager}
+   * to load the {@link #texturePath}.
+   *
+   * @param textureManager {@link TextureManager} : The {@link TextureManager} to
+   *                       use.
+   * @param textureGroup   {@link String} : The group to load the
+   *                       {@link #texturePath} to.
+   */
+  public void load(TextureManager textureManager, String textureGroup) {
+    textureManager.loadAsset(textureGroup, texturePath, "textures");
+  }
 
-	/**
-	 * Returns the {@link Texture} to display with the text.
-	 *
-	 * @return {@link Texture} : The {@link #texture}
-	 */
-	public Texture getTexture() {
-		return texture;
-	}
+  /**
+   * Returns the {@link Texture} to display with the text.
+   *
+   * @return {@link Texture} : The {@link #texture}
+   */
+  public Texture getTexture() {
+    return texture;
+  }
 
-	/**
-	 * Converts the {@link Instruction} into a {@link JsonValue} to
-	 * be stored in memory.
-	 *
-	 * @return {@link JsonValue} : The {@link Instruction} in Json format.
-	 */
-	public JsonValue serial() {
-		JsonValue instructionRoot = new JsonValue(JsonValue.ValueType.object);
-		instructionRoot.addChild("texture_path", new JsonValue(texturePath));
-		instructionRoot.addChild("text", new JsonValue(text));
-		return instructionRoot;
-	}
+  /**
+   * Converts the {@link Instruction} into a {@link JsonValue} to
+   * be stored in memory.
+   *
+   * @return {@link JsonValue} : The {@link Instruction} in Json format.
+   */
+  public JsonValue serial() {
+    JsonValue instructionRoot = new JsonValue(JsonValue.ValueType.object);
+    instructionRoot.addChild("texture_path", new JsonValue(texturePath));
+    instructionRoot.addChild("text", new JsonValue(text));
+    return instructionRoot;
+  }
 
-	/**
-	 * Takes a {@link JsonValue} and sets the values of the
-	 * {@link Instruction}.
-	 *
-	 * @param instructionRoot {@link JsonValue} : The {@link JsonValue} to load.
-	 */
-	public void deserialise(JsonValue instructionRoot) {
-		texturePath = instructionRoot.getString("texture_path");
-		text = instructionRoot.getString("text");
-	}
+  /**
+   * Takes a {@link JsonValue} and sets the values of the
+   * {@link Instruction}.
+   *
+   * @param instructionRoot {@link JsonValue} : The {@link JsonValue} to load.
+   */
+  public void deserialise(JsonValue instructionRoot) {
+    texturePath = instructionRoot.getString("texture_path");
+    text = instructionRoot.getString("text");
+  }
 }
